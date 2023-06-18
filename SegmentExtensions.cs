c73fdfd2bd0be2a8073c417bca3521f288e46e1f
Evgeniyy2004 <evgeniy.ziganshin.04@mail.ru>
2023-06-18 14:59:47 +0500

    Добавьте файлы проекта.

---
+++ SegmentExtensions.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using GeometryTasks;
+
+namespace GeometryPainting
+{
+    //Напишите здесь код, который заставит работать методы segment.GetColor и segment.SetColor
+    static class Extension
+    {
+        public static void SetColor(this Segment seg, Color color)
+        {
+            if (color == null) return;
+            if (!Colors.ContainsKey(seg)) Colors.Add(seg, color);
+            else Colors[seg] = color;
+        }
+
+        public static Color GetColor(this Segment seg)
+        {
+            if (Colors.ContainsKey(seg))
+            {
+                var c = Colors[seg];
+                if (c == Color.Empty || c == null) return Color.Black;
+                return c;
+            }
+            else return Color.Black;
+        }
+
+        public static Dictionary<Segment, Color> Colors = new Dictionary<Segment, Color>();
+    }
+}
